fe705927068c1edeb8889bed45d83dc517181142
Jake McAuley <jje.mcauley@gmail.com>
2022-05-25 10:19:38 -0400

    Added begin functionality

--- content.js
+++ content.js
@@ -1,6 +1,39 @@
+/*
+* Objective list: 25th of may
+* 1) MVP useability. 
+          -Ready state
+          -Intermediate state
+          -Tasks appearing
+          -Button functionality
+*
+* 2) Drop shadow on text
+*/
+
 //Dom Elements
+
+//Text Elements (Circle)
 const main_text = document.querySelectorAll(".text-main");
 const sub_text = document.querySelector(".text-sub");
+const main_text_list = document.querySelectorAll(".text");
+
+//Start Text Elements
+const start_text = document.querySelector(".start-text");
+const start_elements = document.querySelectorAll(".start"); //Everything that needs to be hidden
+
+//Buttons
+const begin_button = document.querySelector(".begin");
+
+const complete_button = document.querySelector(".complete");
+const ignore_button = document.querySelector(".ignore");
+const main_buttons = document.querySelectorAll(".button-main");
+
+begin_button.addEventListener("click", function () {
+  for (element of start_elements) element.classList.add("hidden");
+  setTimeout(() => {
+    for (element of start_elements) element.classList.add("removed");
+    for (element of main_text_list) element.classList.remove("removed");
+  }, 800);
+});
 
 let phase = 1;
 let completedTasks = 0;
